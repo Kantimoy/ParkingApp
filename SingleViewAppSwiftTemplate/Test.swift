015c5c4d2dd184907ab392a8021a8b15547735f1
Kantimoy <kantimoysr@gmail.com>
2019-07-09 06:29:57 -0700

    UmCommented Test case

--- SingleViewAppSwiftTemplate/Test.swift
+++ SingleViewAppSwiftTemplate/Test.swift
@@ -23,27 +23,27 @@ func testForGuest() {
         
         //----------------------First Guest. -- Check for ride and area access.
         
-//                let firstGuest = try Guest(personalInformation: husband, guestType: .classic)
-//                try firstGuest.swipe().canSkipLine
-//                try firstGuest.swipe(area: .kitchArea)
+                let firstGuest = try Guest(personalInformation: husband, guestType: .classic)
+                try firstGuest.swipe().canSkipLine
+                try firstGuest.swipe(area: .kitchArea)
         
         //----------------------Second Guest. -- Check for ride and area access.
-//                let secondGuest = try Guest(personalInformation: wife, guestType: .vip)
-//                try secondGuest.swipe().canSkipLine
-//                try secondGuest.swipe(area: .rideControlArea)
+                let secondGuest = try Guest(personalInformation: wife, guestType: .vip)
+                try secondGuest.swipe().canSkipLine
+                try secondGuest.swipe(area: .rideControlArea)
         
         
         //----------------------Third Guest -- when age above 5 years.
-//                let thirdGuest = try Guest(personalInformation: firstChild, guestType: .freeChild)
-//                try thirdGuest.swipe().canSkipLine
-//                try thirdGuest.swipe(area: .rideControlArea)
+                let thirdGuest = try Guest(personalInformation: firstChild, guestType: .freeChild)
+                try thirdGuest.swipe().canSkipLine
+                try thirdGuest.swipe(area: .rideControlArea)
         
         
         //----------------------Fourth Guest. -- age less than 5 years.
-//                let fourthGuest = try Guest(personalInformation: anotherChild, guestType: .freeChild)
-//                try fourthGuest.swipe().canSkipLine
-//                try fourthGuest.swipe(area: .rideControlArea)
-//                try fourthGuest.getDiscountIn().food
+                let fourthGuest = try Guest(personalInformation: anotherChild, guestType: .freeChild)
+                try fourthGuest.swipe().canSkipLine
+                try fourthGuest.swipe(area: .rideControlArea)
+                try fourthGuest.getDiscountIn().food
         
         
     } catch {
@@ -58,30 +58,30 @@ func testForEmployee() {
     do {
         
         //------------3 Employees ----------
-//        let first = PersonalInformation()
-//        let second = PersonalInformation(firstName: "John", lastName: "Smith", dateOfBirth: nil, streetAddress: "some street", city: "some city", state: "some state", zipCode: "some zipcode")
-//        let third = PersonalInformation(firstName: "Stanley", lastName: "nova", dateOfBirth: nil, streetAddress: "street", city: "city", state: "state", zipCode: "zip")
+        let first = PersonalInformation()
+        let second = PersonalInformation(firstName: "John", lastName: "Smith", dateOfBirth: nil, streetAddress: "some street", city: "some city", state: "some state", zipCode: "some zipcode")
+        let third = PersonalInformation(firstName: "Stanley", lastName: "nova", dateOfBirth: nil, streetAddress: "street", city: "city", state: "state", zipCode: "zip")
         
         
         //----------------------First Employee. -- Personal Info is missing.
         
-        //        let firstEmployee = try Employee(personalInformation: first, employee: .foodService)
-        //        try firstEmployee.swipe().canSkipLine
+                let firstEmployee = try Employee(personalInformation: first, employee: .foodService)
+                try firstEmployee.swipe().canSkipLine
         
         //----------------------Second Employee(rideService) -- Check for ride and area access.
-        //        let secondEmployee = try Employee(personalInformation: second, employee: .rideService)
-        //        try secondEmployee.swipe().canSkipLine
-        //        try print(secondEmployee.swipe(area: .rideControlArea))
-        //        try print(secondEmployee.swipe(area: .kitchArea))
-        //        try print(secondEmployee.getDiscount().onMerchandise)
+                let secondEmployee = try Employee(personalInformation: second, employee: .rideService)
+                try secondEmployee.swipe().canSkipLine
+                try print(secondEmployee.swipe(area: .rideControlArea))
+                try print(secondEmployee.swipe(area: .kitchArea))
+                try print(secondEmployee.getDiscount().onMerchandise)
         
         
         //----------------------Third Employee(maintenence).
-        //        let thirdEmployee = try Employee(personalInformation: third, employee: .maintenence)
-        //        try thirdEmployee.swipe().canSkipLine
-        //        try print(thirdEmployee.swipe(area: .rideControlArea))
-        //        try print(thirdEmployee.swipe(area: .kitchArea))
-        //        try print(thirdEmployee.getDiscount().onFood)
+                let thirdEmployee = try Employee(personalInformation: third, employee: .maintenence)
+                try thirdEmployee.swipe().canSkipLine
+                try print(thirdEmployee.swipe(area: .rideControlArea))
+                try print(thirdEmployee.swipe(area: .kitchArea))
+                try print(thirdEmployee.getDiscount().onFood)
     
     }
     catch {
